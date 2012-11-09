96f4d13222f5cb203d4cf6e9d1da717da7948364
Julian Schrittwieser <j.schrittwieser@gmail.com>
2012-11-09 09:12:06 +0000

    added access restrictions

--- index.py
+++ index.py
@@ -1,16 +1,17 @@
+from functools import wraps
 import commands
 import sqlite3
 import time
-from flask import Flask, render_template, redirect, Response
+from flask import Flask, render_template, redirect, Response, request
 from contextlib import closing
 from remote import Remote
 
 # configuration
 DATABASE = '/home/mononofu/web/web.db'
-DEBUG = False
+DEBUG = True
 SECRET_KET = 'some kasdj234#@#RASf'
-USERNAME = 'admin'
-PASSWORD = 'password'
+USERNAME = 'mononofu'
+PASSWORD = 'alucard'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -24,12 +25,37 @@ def init_db():
             db.cursor().executescript(f.read())
         db.commit()
 
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == USERNAME and password == PASSWORD
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
 @app.route("/")
+@requires_auth
 def hello():
     return render_template('index.html', 
         nas_up = commands.getstatusoutput("fping -c1 -t50 192.168.1.120")[0] == 0)
 
 @app.route("/wake/nas")
+@requires_auth
 def wake_nas():
     return commands.getoutput("wakeonlan 00:25:22:3B:B1:DA")
 
@@ -43,6 +69,7 @@ def receive_ping(host):
     return "ping successful"
 
 @app.route("/list_pings")
+@requires_auth
 def list_pings():
     db = connect_db()
     cur = db.execute('select hostname, timestamp from pings order by timestamp')
@@ -51,6 +78,7 @@ def list_pings():
     return str(pings)    
 
 @app.route("/receiver")
+@requires_auth
 def receiver():
     with Remote() as r:
         return render_template('receiver.html',
@@ -59,6 +87,7 @@ def receiver():
             device = r.get_device())
 
 @app.route("/receiver/vol/<lvl>")
+@requires_auth
 def receiver_set_vol(lvl):
     with Remote() as r:
         r.volume(int(lvl))
@@ -66,6 +95,7 @@ def receiver_set_vol(lvl):
     return redirect('/receiver')
 
 @app.route("/receiver/<cmd>")
+@requires_auth
 def receiver_on(cmd):
     with Remote() as r:
         if cmd == "on": r.on()
@@ -80,10 +110,12 @@ def receiver_on(cmd):
     return redirect('/receiver')
 
 @app.route("/cam")
+@requires_auth
 def cam():
     return render_template('cam.html')
 
 @app.route("/cam/now.jpeg")
+@requires_auth
 def cam_now():
     #commands.getstatusoutput("fswebcam --save cam.jpg -S 20")
     f = open("/media/ramdisk/cam.jpg", "rb")
